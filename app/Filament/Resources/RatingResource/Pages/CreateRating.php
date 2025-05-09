37128a677ad9881adeaa68454ff70b592a86951e
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-09 13:33:40 +0700

    add redirect url

--- app/Filament/Resources/RatingResource/Pages/CreateRating.php
+++ app/Filament/Resources/RatingResource/Pages/CreateRating.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRating extends CreateRecord
 {
     protected static string $resource = RatingResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
