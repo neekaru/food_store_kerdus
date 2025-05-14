768d11f028a3388113e24e9681177eff1ee53ec7
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-14 11:14:19 +0700

    feat(Transaction): add details on transactions

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -201,6 +201,7 @@ class TransactionResource extends Resource
     {
         return [
             'index' => Pages\ListTransactions::route('/'),
+            'view' => Pages\ViewTransaction::route('/{record}')
         ];
     }
 
