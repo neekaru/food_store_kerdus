1cdf422e86f561c6ca8cb4def603406404b2749a
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-14 15:34:26 +0700

    feat(user): add sesuai pedoman

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -57,6 +57,7 @@ class UserResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')->searchable(),
                 Tables\Columns\TextColumn::make('email')->searchable(),
+                Tables\Columns\TextColumn::make('created_at')->dateTime(),
             ])
             ->filters([
                 //
