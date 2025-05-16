f236be0ecdf2a0221a1ad078a7d739c78d506941
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-16 15:36:28 +0700

    feat(auth): add guard customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,13 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Customer extends Model
+class Customer extends Authenticatable
 {
     protected $fillable = [
         'image',
         'name',
         'email',
+        'password',
+    ];
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Hidden Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The attributes that should be hidden for serialization.
+    |
+    */
+    protected $hidden = [
         'password',
     ];
 }
