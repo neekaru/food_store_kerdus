b495b473cab662566a388229a8b167272e980aec
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-16 15:11:36 +0700

    feat(layout): add bottom menu

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -24,6 +24,10 @@
     {{ $slot }}
     <!-- end render content -->
 
+    <!-- bottom menu -->
+    <x-menus.bottom />
+    <!-- end bottom menu -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
