8284b592c582ab43a8173b45cd297b11f0f83095
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-16 15:50:49 +0700

    feat(livewire): add component register

---
+++ app/Livewire/Auth/Register.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Livewire\Component;
+use App\Models\Customer;
+
+
+class Register extends Component
+{
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    public function rules()
+    {
+        return [
+            'name' => ['required'],
+            'email' => ['required', 'email', 'unique:customers,email'],
+            'password' => ['required', 'confirmed'],
+        ];
+    }
+
+    public function register()
+    {
+        // for validation
+        $this->validate();
+
+        // Create Customer
+        Customer::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+
+        session()->flash('success', 'Register Berhasil, silahkan login');
+
+        return $this->redirect('/login', navigate: true);
+    }
+
+    public function render()
+    {
+        return view('livewire.auth.register');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+namespace App\Livewire;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', Auth\Register::class)->name('register');
