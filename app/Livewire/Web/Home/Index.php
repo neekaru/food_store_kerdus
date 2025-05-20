b45c938a12194b5d0d96b69e161414243a7ad1cc
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-20 10:38:09 +0700

    feat(product): add product popular

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -3,16 +3,27 @@
 namespace App\Livewire\Web\Home;
 
 use App\Models\Slider;
+use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
 
 class Index extends Component
 {
+    protected function getPopularProducts()
+    {
+        return Product::with('category', 'ratings.customer')
+            ->withAvg('ratings', 'rating') // menghitung rata-rata rating
+            ->having('ratings_avg_rating', '>=', 4)
+            ->limit(5)
+            ->get();
+    }
+
     public function render()
     {
         return view('livewire.web.home.index', [
             'sliders' => Slider::latest()->get(),
             'categories' => Category::latest()->get(),
+            'popularProducts' => $this->getPopularProducts()
         ]);
     }
 }
