363145a67f4d69271d7c94160304bd3dffbdb5c5
nekaru <32153003+ninajika@users.noreply.github.com>
2025-05-20 09:36:44 +0700

    feat categories and fix layout issue

--- app/Livewire/Web/Home/Index.php
+++ app/Livewire/Web/Home/Index.php
@@ -3,11 +3,14 @@
 namespace App\Livewire\Web\Home;
 
 use Livewire\Component;
+use App\Models\Category;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.web.home.index');
+        return view('livewire.web.home.index', [
+            'categories' => Category::latest()->get(),
+        ]);
     }
 }
---
+++ resources/views/components/cards/category.blade.php
@@ -0,0 +1,8 @@
+<div class="col-6 col-md-4 mb-4">
+    <a href="/category/{{ $category->slug }}" wire:navigate class="text-decoration-none">
+        <div class="card-body text-center">
+            <img src="{{ asset('/storage/' . $category->image) }}" class="img-fluid" width="50">
+            <label class="text-center">{{  $category->name }}</label>
+        </div>
+    </a>
+</div>
