6e7ce365568c0f707a222c5bd8455949c1a64997
TailsX2 <88992905+TailsX2@users.noreply.github.com>
2025-05-24 08:32:16 +0700

    a

--- resources/views/livewire/web/home/index.blade.php
+++ resources/views/livewire/web/home/index.blade.php
@@ -1,5 +1,5 @@
 @section('title')
-Food Store - Eat Your Favorite Foods
+Carts - Eat Your Favorite Foods
 @stop
 
 @section('keywords')
@@ -15,117 +15,105 @@ Food Store - Eat Your Favorite Foods
 @stop
 
 <div>
-    <div class="container" style="margin-bottom: 120px">
-        
-        <!-- Search Bar -->
-        <x-utils.search-bar />
 
-        <!-- Sliders -->
-        <div class="row justify-content-center mt-4">
+    <div class="container">
+        <div class="row justify-content-center mt-0" style="margin-bottom: 270px;">
             <div class="col-md-6">
-                <div id="carouselExample" class="carousel slide w-100">
-                    <div class="carousel-inner">
-        
-                        @foreach ($sliders as $key => $slider)
-                            <div class="carousel-item @if($key == 0) active @endif">
-                                <img src="{{ asset('/storage/' . $slider->image) }}" class="d-block w-100 rounded">
-                            </div>
-                        @endforeach
-        
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-
-        <!-- Categories -->
-        <div class="row justify-content-center mt-4 mb-5">
-            <div class="col-md-6">
-                <span class="fs-6 fw-bold">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box mb-1" viewBox="0 0 16 16">
-                        <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z" />
-                    </svg>
-                    CATEGORIES
-                </span>
-                <hr />
-                <div class="row flex-nowrap overflow-auto scroll-custom">
-                    @foreach ($categories as $category)
-                        <x-cards.category :category="$category" />
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        <!-- Products Popular -->
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-6">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <span class="fs-6 fw-bold">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag mb-1" viewBox="0 0 16 16">
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
-                            </svg>
-                            PRODUCTS <span class="text-orange">POPULER</span>
-                        </span>
-                    </div>
-                    <div>
-                        <a href="/products" wire:navigate class="text-decoration-none text-orange fw-bold">
-                            Lihat Lainnya
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                            </svg>
-                        </a>
+                <div class="bg-white rounded-bottom-custom shadow-sm p-3 sticky-top mb-5">
+                    <div class="d-flex justify-content-start">
+                        <div>
+                            <x-buttons.back />
+                        </div>
                     </div>
                 </div>
 
-                <hr />
-                <div class="row flex-nowrap overflow-auto scroll-custom">
-                    @foreach ($popularProducts as $product)
-                        <x-cards.product-popular :product="$product" />
-                    @endforeach
-                </div>
-            </div>
-        </div>
-
-        <!-- Products Latest -->
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-6">
-                <div class="d-flex justify-content-between">
-                    <div>
-                        <span class="fs-6 fw-bold">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag mb-1" viewBox="0 0 16 16">
-                                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
-                            </svg>
-                            PRODUCTS <span class="text-orange">TERBARU</span>
-                        </span>
-                    </div>
-                    <div>
-                        <a href="/products" wire:navigate class="text-decoration-none text-orange fw-bold">
-                            Lihat Lainnya
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8" />
-                            </svg>
-                        </a>
-                    </div>
-                </div>
-                <hr />
                 <div class="row">
-                    <div class="col-12 col-md-12 mb-2">
-                        @foreach ($latestProducts as $product)
-                            <x-cards.product :product="$product" />
-                        @endforeach
-                    </div>
+
+                    @forelse ($carts as $cart)
+                        <div class="col-12 col-md-12 mb-4">
+                            <div class="card rounded border-0 shadow-sm">
+                                <div class="row g-0">
+                                    <div class="col-5 col-md-4">
+                                        <img src="{{ asset('/storage/' . $cart->product->image) }}" class="img-fluid w-100 h-100 object-fit-cover rounded-start">
+                                    </div>
+                                    <div class="col-7 col-md-8">
+                                        <div class="card-body">
+                                            <div class="d-flex justify-content-between mt-4">
+                                                <div class="text-start">
+                                                    <h6 class="card-title">{{ $cart->product->title }}</h6>
+                                                </div>
+                                                <div class="text-end">
+                                                    
+                                                    <!-- btn delete -->
+                                                    <livewire:web.cart.btn-delete :cart_id="$cart->id" />
+
+                                                </div>
+                                            </div>
+                                            <div class="d-flex justify-content-between mt-4">
+                                                <div class="text-start">
+                                                    <span class="text-success fw-bold">Rp. {{ number_format($cart->product->price) }}</span>
+                                                </div>
+                                                <div class="text-end">
+                                                    <div class="input-group justify-content-center align-items-center group-btn-qty">
+                                                        
+                                                        <!-- decrement qty -->
+                                                        <livewire:web.cart.btn-decrement :cart_id="$cart->id" :product_id="$cart->product_id" :disabled="$cart->qty" />
+
+                                                        <!-- qty cart -->
+                                                        <input type="number" step="1" max="10" value="{{ $cart->qty }}" name="quantity" class="quantity-field border-0 text-center w-25" style="background: transparent;">
+                                                        
+                                                        <!-- increment qty -->
+                                                        <livewire:web.cart.btn-increment :cart_id="$cart->id" :product_id="$cart->product_id" />
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="col-12 col-md-12 mb-4">
+                            <div class="card rounded border mb-3">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-center">
+                                        <div class="mt-2">
+                                            <span class="fw-bold">You don't have any items in the cart.</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforelse
+
                 </div>
+
             </div>
         </div>
     </div>
+
+    @if(count($carts) > 0)
+        <div class="container fixed-total">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-6">
+                    <div class="card rounded shadow-sm border-0 mb-5 ">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <h6 class="fw-bold mb-0">Total</h6>
+                                </div>
+                                <div class="ms-auto">
+                                    <h6 class="fw-bold mb-0">Rp. {{ number_format($totalPrice) }}</h6>
+                                </div>
+                            </div>
+                            <hr style="border: dotted 1px #e92715;">
+                            <a href="/checkout" wire:navigate class="btn btn-orange-2 rounded border-0 shadow-sm w-100">Process to Checkout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
+
 </div>
